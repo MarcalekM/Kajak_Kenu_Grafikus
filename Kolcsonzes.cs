7a2a0cd376f665cfd28461b9815181bda09e3421
Sufi <120452154+MarcalekM@users.noreply.github.com>
2025-01-27 10:23:22 +0100

    Add project files.

---
+++ Kolcsonzes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kajak_Kenu_Grafikus
+{
+    class Kolcsonzes
+    {
+        public string Nev { get; set; }
+        public int HajoAzonosito { get; set; }
+        public string HajoTipusa { get; set; }
+        public int SzemelyekSzama { get; set; }
+        public int ElvitelOra { get; set; }
+        public int ElvitelPerc { get; set; }
+        public int VisszahozatalOra { get; set; }
+        public int VisszahozatalPerc { get; set; }
+
+        public bool VizenVan(string ido)
+        {
+            int ora = int.Parse(ido.Split(':')[0]);
+            int perc = int.Parse(ido.Split(':')[1]);
+            if (ElvitelOra == ora && ElvitelPerc < perc && (VisszahozatalOra > ora || VisszahozatalOra == ora && VisszahozatalPerc >= perc)) return true;
+            else return false;
+        }
+
+        public string KolcsonzesIdotartam()
+        {
+            return $"{VisszahozatalOra - ElvitelOra} óra {VisszahozatalPerc - ElvitelPerc} perc";
+        }
+
+        public Kolcsonzes(string sor)
+        {
+            var temp = sor.Split(';').ToList();
+            Nev = temp[0];
+            HajoAzonosito = int.Parse(temp[1]);
+            HajoTipusa = temp[2];
+            SzemelyekSzama = int.Parse(temp[3]);
+            ElvitelOra = int.Parse(temp[4]);
+            ElvitelPerc = int.Parse(temp[5]);
+            VisszahozatalOra = int.Parse(temp[6]);
+            VisszahozatalPerc = int.Parse(temp[7]);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Kajak_Kenu_Grafikus;
+
+
+namespace Kajak_Kenu_Grafikus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Kolcsonzes> kolcsonzesek = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using StreamReader sr = new(
+                path: @"../../../src/kolcsonzes.txt",
+                encoding: Encoding.UTF8);
+            _ = sr.ReadLine();
+            while (!sr.EndOfStream) kolcsonzesek.Add(new(sr.ReadLine()));
+            foreach(var kolcsonzes in kolcsonzesek) Kolcsonzesek_Grid.Items.Add(kolcsonzes);
+
+            for(int i = 0; i < 24; i++) Ora.Items.Add(i);
+            for (int i = 0; i < 60; i++) Perc.Items.Add(i);
+        }
+
+        private void Keres_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
